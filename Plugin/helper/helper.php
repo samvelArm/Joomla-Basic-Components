8d912d63f8689561d446655e3c6a5300115c9c9d
Samvel Antanyan <samvelantanyan@workfront.com>
2016-04-28 12:54:16 +0400

    Added Basic Plugin Structure With Dummy Plugin Skeleton

---
+++ Plugin/helper/helper.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package Joomla.Plugin
+ * @subpackage Content.joomla
+ *
+ * @copyright Copyright (C) 2016 Creative Solutions. All Rights Reserved.
+ * NGU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+
+class PluginNameHelper
+{
+    public $params = null;
+
+    public function  __construct($params = null)
+    {
+        $this->params = $params;
+    }
+
+
+}
--- Plugin/pluginname.php
+++ Plugin/pluginname.php
@@ -1,7 +1,49 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: samvelantanyan
- * Date: 4/25/16
- * Time: 2:22 AM
+ * @package Joomla.Plugin
+ * @subpackage Content.joomla
+ *
+ * @copyright Copyright (C) 2016 Creative Solutions. All Rights Reserved.
+ * NGU General Public License version 2 or later; see LICENSE.txt
  */
+
+defined('_JEXEC') or die;
+
+
+
+class PlgContentPluginName extends Jplugin
+{
+   public function __construct(&$subject, $config=array())
+   {
+       parent::__construct($subject, $config);  // Run the parent constructor
+
+       require_once __DIR__. '/helper/helper.php';
+
+       $helper = new PluginNameHelper($this->params); //params defined in xml file to config from admin panel
+
+   }
+    /**
+     * Plugin events list is here https://docs.joomla.org/Plugin/Events
+     *
+     * onContentBeforeDisplay initiate the plugin
+     * @param string $context the context of the content passes to the plugin
+     * @param object $article the article object
+     * @param object $params the article params
+     *
+     * @return boolean true if function bypassed else true/false based on replacement action
+     */
+    public function onContentBeforeDisplay($context, $article, $params)
+    {
+        $parts = explode(".", $context);
+        if ($parts[0]!= "com_content")
+        {
+            return;
+        }
+
+        // All the job goes here
+
+        $app = JFactory::getApplication();
+
+
+    }
+}
